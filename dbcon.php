fda9f9e7c40bec78b4a71c45e6b02a1a36a8f545
Shantanu Khandelwal <shantanukhandelwal8@gmail.com>
2021-10-13 15:05:21 +0530

    database remote

--- dbcon.php
+++ dbcon.php
@@ -1,4 +1,4 @@
 <?php
-        $con = mysqli_connect("localhost", "root", "", "bingefy")
+        $con = mysqli_connect("remotemysql.com", "JOSMTYV8va", "SB7DzMxVJP", "JOSMTYV8va")
                 or die(mysqli_error($con));
 ?>
